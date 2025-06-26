73896df03f8e74b1d20e55da6376a3ebd6db10f0
Evald-as <evaldasbuz@gmail.com>
2025-06-26 13:13:03 +0000

    Deploying to gh-pages from @ Evald-as/docs@0c75714efa4e09f63f8dc1a34fd7ff27a48b4a21 🚀

---
+++ _nuxt/R9RLbShR.js
@@ -0,0 +1,1 @@
+import{r as t}from"./C25zddV1.js";import e from"./CUOvuKud.js";import{z as n,H as s,i as a,w as i,W as p}from"./C8WbRjLk.js";import"./C2bHYaEW.js";const d={__name:"ProseNote",setup(c){const o=n();return(r,f)=>(a(),s(e,{color:"info",icon:p(o).ui.icons.info},{default:i(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
