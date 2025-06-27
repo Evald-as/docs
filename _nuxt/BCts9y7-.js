6c38be77e5b5cb661337276477ba2a4cbb0cd29f
Evald-as <evaldasbuz@gmail.com>
2025-06-27 19:06:52 +0000

    Deploying to gh-pages from @ Evald-as/docs@2bba5ff8532bbc5546fa2c14625d849d31a9f7f6 🚀

---
+++ _nuxt/BCts9y7-.js
@@ -0,0 +1,1 @@
+import{r as t}from"./Bw7kgKBW.js";import a from"./Mae2lIIt.js";import{z as e,H as n,i as s,w as i,W as c}from"./3lvB0PZd.js";import"./C2bHYaEW.js";const d={__name:"ProseCaution",setup(p){const o=e();return(r,f)=>(s(),n(a,{color:"error",icon:c(o).ui.icons.caution},{default:i(()=>[t(r.$slots,"default",{mdcUnwrap:"p"})]),_:3},8,["icon"]))}};export{d as default};
